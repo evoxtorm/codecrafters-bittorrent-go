626d6c7faf799de9e43a42d5df1695dbb44dbf6c
evoxtorm <evostorm96@gmail.com>
2023-09-18 11:04:45 +0530

    feat: add code for stage 10

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -1,3 +1,254 @@
+// package main
+
+// import (
+// 	"bytes"
+// 	"crypto/sha1"
+// 	"encoding/binary"
+// 	"encoding/hex"
+// 	"encoding/json"
+// 	"fmt"
+// 	"io"
+// 	"net"
+// 	"net/http"
+// 	"net/url"
+// 	"os"
+// 	"strconv"
+
+// 	bencode "github.com/jackpal/bencode-go"
+// )
+
+// type Torrent struct {
+// 	Announce string `bencode:"announce"`
+// 	Info     Info   `bencode:"info"`
+// }
+
+// type Info struct {
+// 	Length    int64  `bencode:"length"`
+// 	Name      string `bencode:"name"`
+// 	PiecesLen int64  `bencode:"piece length"`
+// 	Pieces    string `bencode:"pieces"`
+// }
+
+// type TrackerResponse struct {
+// 	Interval int64  `bencode:"interval"`
+// 	Peers    string `bencode:"peers"`
+// }
+
+// func splitString(input string, chunkSize int) []string {
+// 	var chunks []string
+
+// 	for i := 0; i < len(input); i += chunkSize {
+// 		end := i + chunkSize
+// 		if end > len(input) {
+// 			end = len(input)
+// 		}
+// 		chunk := input[i:end]
+// 		chunks = append(chunks, chunk)
+// 	}
+
+// 	return chunks
+// }
+
+// func get_request(jsonObject Torrent, buffer_ bytes.Buffer) (bool, error) {
+// 	url_ := jsonObject.Announce
+// 	info_hash := sha1.Sum(buffer_.Bytes())
+// 	queryParams := url.Values{}
+// 	queryParams.Add("info_hash", string(info_hash[:]))
+// 	queryParams.Add("peer_id", "00112233445566778899")
+// 	queryParams.Add("port", "6881")
+// 	queryParams.Add("uploaded", "0")
+// 	queryParams.Add("downloaded", "0")
+// 	queryParams.Add("left", strconv.Itoa(int(jsonObject.Info.Length)))
+// 	queryParams.Add("compact", "1")
+// 	encodedParams := queryParams.Encode()
+// 	fullURL := fmt.Sprintf("%s?%s", url_, encodedParams)
+
+// 	response, err := http.Get(fullURL)
+// 	if err != nil {
+// 		fmt.Println("Error:", err)
+// 		return false, err
+// 	}
+// 	defer response.Body.Close()
+
+// 	if response.StatusCode != http.StatusOK {
+// 		fmt.Println("Request failed with status code:", response.StatusCode)
+// 		return false, err
+// 	}
+
+// 	var trackerResponse TrackerResponse
+// 	err = bencode.Unmarshal(response.Body, &trackerResponse)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return false, err
+// 	}
+// 	peerSize := 6
+// 	numPeers := len(trackerResponse.Peers) / peerSize
+// 	for i := 0; i < numPeers; i++ {
+// 		start := i * 6
+// 		end := start + 6
+// 		peer := trackerResponse.Peers[start:end]
+// 		ip := net.IP(peer[0:4])
+// 		port := binary.BigEndian.Uint16([]byte(peer[4:6]))
+// 		fmt.Printf("%s:%d\n", ip, port)
+// 	}
+// 	return true, nil
+// }
+
+// func sendHandshake(peers string, buffer bytes.Buffer) {
+// 	conn, err := net.Dial("tcp", peers)
+// 	if err != nil {
+// 		fmt.Printf("Error while making connection: %s\n", err)
+// 		return
+// 	}
+// 	defer conn.Close()
+
+// 	// Prepare infoHash and peerID
+// 	infoHash := sha1.Sum(buffer.Bytes())
+// 	peerID := []byte("00112233445566778899")
+
+// 	// Build the handshake message
+// 	handshake := new(bytes.Buffer)
+// 	handshake.WriteByte(19)
+// 	handshake.WriteString("BitTorrent protocol")
+// 	handshake.Write([]byte{0, 0, 0, 0, 0, 0, 0, 0}) // 8 reserved bytes
+// 	handshake.Write(infoHash[:])
+// 	handshake.Write(peerID)
+
+// 	// Send the handshake message over the connection
+// 	_, err = conn.Write(handshake.Bytes())
+// 	if err != nil {
+// 		fmt.Println("Error sending handshake:", err)
+// 		return
+// 	}
+
+// 	// Read and process the response handshake
+// 	buf := make([]byte, 68)
+// 	_, err = io.ReadFull(conn, buf)
+// 	if err != nil {
+// 		if err == io.EOF {
+// 			fmt.Println("Peer closed the connection")
+// 		} else {
+// 			fmt.Println("Error reading response:", err)
+// 		}
+// 		return
+// 	}
+
+// 	// Extract and print the peer id from the response handshake
+// 	receivedPeerID := buf[48:]
+// 	fmt.Printf("Peer ID: %s\n", hex.EncodeToString(receivedPeerID))
+// }
+
+// func main() {
+// 	command := os.Args[1]
+// 	if command == "decode" {
+// 		bencodedValue := os.Args[2]
+// 		decoded, err := bencode.Decode(bytes.NewReader([]byte(bencodedValue)))
+// 		if err != nil {
+// 			fmt.Println(err)
+// 			return
+// 		}
+// 		jsonOutput, _ := json.Marshal(decoded)
+// 		fmt.Println(string(jsonOutput))
+// 	} else if command == "info" {
+// 		filename := os.Args[2]
+// 		data, err := os.Open(filename)
+// 		if err != nil {
+// 			fmt.Println("Error reading file:", err)
+// 			return
+// 		}
+// 		defer data.Close()
+// 		if err != nil {
+// 			fmt.Println(err)
+// 			return
+// 		}
+// 		jsonObject := Torrent{}
+// 		err = bencode.Unmarshal(data, &jsonObject)
+// 		if err != nil {
+// 			fmt.Println(err)
+// 			return
+// 		}
+
+// 		var buffer_ bytes.Buffer
+
+// 		if err := bencode.Marshal(&buffer_, jsonObject.Info); err != nil {
+// 			return
+// 		}
+// 		chunkSize := 20
+
+// 		chunks := splitString(jsonObject.Info.Pieces, chunkSize)
+// 		fmt.Printf("Tracker URL: %s\n", jsonObject.Announce)
+// 		fmt.Printf("Length: %d\n", jsonObject.Info.Length)
+// 		fmt.Printf("Info Hash: %x\n", sha1.Sum(buffer_.Bytes()))
+// 		fmt.Printf("Piece Length: %d\n", jsonObject.Info.PiecesLen)
+// 		fmt.Printf("Piece Hashes:\n")
+// 		for _, chunk := range chunks {
+// 			if command == "info" {
+// 				fmt.Printf("%x\n", chunk)
+// 			}
+// 		}
+
+// 	} else if command == "peers" {
+// 		filename := os.Args[2]
+// 		data, err := os.Open(filename)
+// 		if err != nil {
+// 			fmt.Println("Error reading file:", err)
+// 			return
+// 		}
+// 		defer data.Close()
+// 		if err != nil {
+// 			fmt.Println(err)
+// 			return
+// 		}
+// 		jsonObject := Torrent{}
+// 		err = bencode.Unmarshal(data, &jsonObject)
+// 		if err != nil {
+// 			fmt.Println(err)
+// 			return
+// 		}
+
+// 		var buffer_ bytes.Buffer
+
+// 		if err := bencode.Marshal(&buffer_, jsonObject.Info); err != nil {
+// 			return
+// 		}
+// 		get_request(jsonObject, buffer_)
+// 	} else if command == "handshake" {
+// 		filename := os.Args[2]
+// 		data, err := os.Open(filename)
+// 		if err != nil {
+// 			fmt.Println("Error reading file:", err)
+// 			return
+// 		}
+// 		defer data.Close()
+// 		if err != nil {
+// 			fmt.Println(err)
+// 			return
+// 		}
+// 		jsonObject := Torrent{}
+// 		err = bencode.Unmarshal(data, &jsonObject)
+// 		if err != nil {
+// 			fmt.Println(err)
+// 			return
+// 		}
+
+// 		var buffer_ bytes.Buffer
+
+// 		if err := bencode.Marshal(&buffer_, jsonObject.Info); err != nil {
+// 			return
+// 		}
+// 		peers := os.Args[3]
+// 		// val := strings.Split(peers, ":")
+// 		// ip := val[0]
+// 		// portt := val[1]
+// 		sendHandshake(peers, buffer_)
+// 		// conn.Close()
+
+// 	} else {
+// 		fmt.Println("Unknown command: " + command)
+// 		os.Exit(1)
+// 	}
+// }
+
 package main
 
 import (
@@ -34,6 +285,20 @@ type TrackerResponse struct {
 	Peers    string `bencode:"peers"`
 }
 
+type Peers struct {
+	Ip   net.IP
+	Port uint64
+}
+
+const (
+	Bitfield   = 5
+	Interested = 2
+	Unchoke    = 1
+	Request    = 6
+	Piece      = 7
+	BLOCK      = 16 * 1024
+)
+
 func splitString(input string, chunkSize int) []string {
 	var chunks []string
 
@@ -49,11 +314,10 @@ func splitString(input string, chunkSize int) []string {
 	return chunks
 }
 
-func get_request(jsonObject Torrent, buffer_ bytes.Buffer) (bool, error) {
-	url_ := jsonObject.Announce
-	info_hash := sha1.Sum(buffer_.Bytes())
+func getRequest(jsonObject Torrent, buffer_ bytes.Buffer) (TrackerResponse, error) {
+	infoHash := sha1.Sum(buffer_.Bytes())
 	queryParams := url.Values{}
-	queryParams.Add("info_hash", string(info_hash[:]))
+	queryParams.Add("info_hash", string(infoHash[:]))
 	queryParams.Add("peer_id", "00112233445566778899")
 	queryParams.Add("port", "6881")
 	queryParams.Add("uploaded", "0")
@@ -61,52 +325,34 @@ func get_request(jsonObject Torrent, buffer_ bytes.Buffer) (bool, error) {
 	queryParams.Add("left", strconv.Itoa(int(jsonObject.Info.Length)))
 	queryParams.Add("compact", "1")
 	encodedParams := queryParams.Encode()
-	fullURL := fmt.Sprintf("%s?%s", url_, encodedParams)
+	fullURL := fmt.Sprintf("%s?%s", jsonObject.Announce, encodedParams)
 
 	response, err := http.Get(fullURL)
 	if err != nil {
 		fmt.Println("Error:", err)
-		return false, err
+		return TrackerResponse{}, err
 	}
 	defer response.Body.Close()
 
 	if response.StatusCode != http.StatusOK {
 		fmt.Println("Request failed with status code:", response.StatusCode)
-		return false, err
+		return TrackerResponse{}, err
 	}
 
 	var trackerResponse TrackerResponse
 	err = bencode.Unmarshal(response.Body, &trackerResponse)
 	if err != nil {
 		fmt.Println(err)
-		return false, err
+		return TrackerResponse{}, err
 	}
-	peerSize := 6
-	numPeers := len(trackerResponse.Peers) / peerSize
-	for i := 0; i < numPeers; i++ {
-		start := i * 6
-		end := start + 6
-		peer := trackerResponse.Peers[start:end]
-		ip := net.IP(peer[0:4])
-		port := binary.BigEndian.Uint16([]byte(peer[4:6]))
-		fmt.Printf("%s:%d\n", ip, port)
-	}
-	return true, nil
+
+	return trackerResponse, nil
 }
 
-func sendHandshake(peers string, buffer bytes.Buffer) {
-	conn, err := net.Dial("tcp", peers)
-	if err != nil {
-		fmt.Printf("Error while making connection: %s\n", err)
-		return
-	}
-	defer conn.Close()
-
-	// Prepare infoHash and peerID
+func sendHandshake(conn net.Conn, peers string, buffer bytes.Buffer) {
 	infoHash := sha1.Sum(buffer.Bytes())
 	peerID := []byte("00112233445566778899")
 
-	// Build the handshake message
 	handshake := new(bytes.Buffer)
 	handshake.WriteByte(19)
 	handshake.WriteString("BitTorrent protocol")
@@ -114,14 +360,12 @@ func sendHandshake(peers string, buffer bytes.Buffer) {
 	handshake.Write(infoHash[:])
 	handshake.Write(peerID)
 
-	// Send the handshake message over the connection
-	_, err = conn.Write(handshake.Bytes())
+	_, err := conn.Write(handshake.Bytes())
 	if err != nil {
 		fmt.Println("Error sending handshake:", err)
 		return
 	}
 
-	// Read and process the response handshake
 	buf := make([]byte, 68)
 	_, err = io.ReadFull(conn, buf)
 	if err != nil {
@@ -133,14 +377,91 @@ func sendHandshake(peers string, buffer bytes.Buffer) {
 		return
 	}
 
-	// Extract and print the peer id from the response handshake
 	receivedPeerID := buf[48:]
 	fmt.Printf("Peer ID: %s\n", hex.EncodeToString(receivedPeerID))
 }
 
+func get_peers(trackerResponse TrackerResponse) []Peers {
+	peerSize := 6
+	numPeers := len(trackerResponse.Peers) / peerSize
+	peersArray := make([]Peers, numPeers)
+	for i := 0; i < numPeers; i++ {
+		start := i * 6
+		end := start + 6
+		peer := trackerResponse.Peers[start:end]
+		ip := net.IP(peer[0:4])
+		port := binary.BigEndian.Uint16([]byte(peer[4:6]))
+		peersArray[i] = Peers{
+			Ip:   ip,
+			Port: uint64(port),
+		}
+	}
+	return peersArray
+}
+
+func printPeers(peers []Peers) {
+	for i := 0; i < len(peers); i++ {
+		fmt.Printf("%s:%d\n", peers[i].Ip, peers[i].Port)
+	}
+}
+
+func handlePeerMessages(conn net.Conn, messageID_ uint8) []byte {
+	buffer := make([]byte, 4)
+	for {
+		_, err := io.ReadFull(conn, buffer)
+		if (err) != nil {
+			fmt.Println("Error reading message length:", err)
+			return nil
+		}
+		messageID := make([]byte, 1)
+		_, err = io.ReadFull(conn, messageID)
+		if err != nil {
+			fmt.Println("Error reading message ID:", err)
+			return nil
+		}
+		messageLength := binary.BigEndian.Uint32(buffer)
+		payload := make([]byte, messageLength-1)
+
+		_, err = io.ReadFull(conn, payload)
+		if err != nil {
+			fmt.Println("Error reading message length:", err)
+			return nil
+		}
+
+		if messageID[0] == messageID_ {
+			return payload
+		}
+	}
+}
+
+func createConnection(peer string) (net.Conn, error) {
+	conn, err := net.Dial("tcp", peer)
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
+}
+
+func closeALlConn(conns map[string]net.Conn) {
+	for _, conn := range conns {
+		conn.Close()
+	}
+}
+
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: ./your_bittorrent.sh <command>")
+		return
+	}
+
 	command := os.Args[1]
-	if command == "decode" {
+
+	switch command {
+	case "decode":
+		if len(os.Args) < 3 {
+			fmt.Println("Usage: ./your_bittorrent.sh decode <bencoded_value>")
+			return
+		}
 		bencodedValue := os.Args[2]
 		decoded, err := bencode.Decode(bytes.NewReader([]byte(bencodedValue)))
 		if err != nil {
@@ -149,7 +470,12 @@ func main() {
 		}
 		jsonOutput, _ := json.Marshal(decoded)
 		fmt.Println(string(jsonOutput))
-	} else if command == "info" {
+
+	case "info", "peers", "handshake":
+		if len(os.Args) < 3 {
+			fmt.Println("Usage: ./your_bittorrent.sh", command, "<torrent_file>")
+			return
+		}
 		filename := os.Args[2]
 		data, err := os.Open(filename)
 		if err != nil {
@@ -157,10 +483,7 @@ func main() {
 			return
 		}
 		defer data.Close()
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
+
 		jsonObject := Torrent{}
 		err = bencode.Unmarshal(data, &jsonObject)
 		if err != nil {
@@ -173,32 +496,54 @@ func main() {
 		if err := bencode.Marshal(&buffer_, jsonObject.Info); err != nil {
 			return
 		}
-		chunkSize := 20
 
-		chunks := splitString(jsonObject.Info.Pieces, chunkSize)
-		fmt.Printf("Tracker URL: %s\n", jsonObject.Announce)
-		fmt.Printf("Length: %d\n", jsonObject.Info.Length)
-		fmt.Printf("Info Hash: %x\n", sha1.Sum(buffer_.Bytes()))
-		fmt.Printf("Piece Length: %d\n", jsonObject.Info.PiecesLen)
-		fmt.Printf("Piece Hashes:\n")
-		for _, chunk := range chunks {
-			if command == "info" {
+		switch command {
+		case "info":
+			chunkSize := 20
+			chunks := splitString(jsonObject.Info.Pieces, chunkSize)
+			fmt.Printf("Tracker URL: %s\n", jsonObject.Announce)
+			fmt.Printf("Length: %d\n", jsonObject.Info.Length)
+			fmt.Printf("Info Hash: %x\n", sha1.Sum(buffer_.Bytes()))
+			fmt.Printf("Piece Length: %d\n", jsonObject.Info.PiecesLen)
+			fmt.Printf("Piece Hashes:\n")
+			for _, chunk := range chunks {
 				fmt.Printf("%x\n", chunk)
 			}
-		}
 
-	} else if command == "peers" {
-		filename := os.Args[2]
+		case "peers":
+			trackerResponse, err := getRequest(jsonObject, buffer_)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			peers := get_peers(trackerResponse)
+			printPeers(peers)
+
+		case "handshake":
+			if len(os.Args) < 4 {
+				fmt.Println("Usage: ./your_bittorrent.sh handshake <torrent_file> <peer_ip:peer_port>")
+				return
+			}
+			peers := os.Args[3]
+			conn, err := createConnection(peers)
+			if err != nil {
+				return
+			}
+			sendHandshake(conn, peers, buffer_)
+		}
+	case "download_piece":
+		filename := os.Args[4]
 		data, err := os.Open(filename)
 		if err != nil {
 			fmt.Println("Error reading file:", err)
 			return
 		}
-		defer data.Close()
+		pieceIndex, err := strconv.Atoi(os.Args[5])
 		if err != nil {
-			fmt.Println(err)
 			return
 		}
+		defer data.Close()
+
 		jsonObject := Torrent{}
 		err = bencode.Unmarshal(data, &jsonObject)
 		if err != nil {
@@ -211,40 +556,87 @@ func main() {
 		if err := bencode.Marshal(&buffer_, jsonObject.Info); err != nil {
 			return
 		}
-		get_request(jsonObject, buffer_)
-	} else if command == "handshake" {
-		filename := os.Args[2]
-		data, err := os.Open(filename)
-		if err != nil {
-			fmt.Println("Error reading file:", err)
-			return
-		}
-		defer data.Close()
+		trackerResponse, err := getRequest(jsonObject, buffer_)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-		jsonObject := Torrent{}
-		err = bencode.Unmarshal(data, &jsonObject)
+		connections := map[string]net.Conn{}
+		// sendHandshake(peers, buffer_)
+		peers := get_peers(trackerResponse)
+		defer closeALlConn(connections)
+		peerObjVal := peers[0]
+		peerStr := fmt.Sprintf("%s:%d\n", peerObjVal.Ip, peerObjVal.Port)
+		connections[peerStr], err = createConnection(peerStr)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-
-		var buffer_ bytes.Buffer
-
-		if err := bencode.Marshal(&buffer_, jsonObject.Info); err != nil {
-			return
+		sendHandshake(connections[peerStr], peerStr, buffer_)
+		handlePeerMessages(connections[peerStr], Bitfield)
+		interestedMessage := []byte{0, 0, 0, 1, 2} // Message length (1 byte) + Message ID (1 byte) + Payload (empty)
+		connections[peerStr].Write(interestedMessage)
+		handlePeerMessages(connections[peerStr], Unchoke)
+		piecesHex := jsonObject.Info.Pieces
+		pieces := make([]string, len(piecesHex)/20)
+		for i := 0; i < len(pieces); i++ {
+			start := i * 20
+			end := (i + 1) * 20
+			if end > len(piecesHex) {
+				end = len(piecesHex)
+			}
+			pieceHex := piecesHex[start:end]
+			pieces[i] = hex.EncodeToString([]byte(pieceHex)) // Convert bytes to hexadecimal string
 		}
-		peers := os.Args[3]
-		// val := strings.Split(peers, ":")
-		// ip := val[0]
-		// portt := val[1]
-		sendHandshake(peers, buffer_)
-		// conn.Close()
+		piecesHash := pieces[pieceIndex]
+		numBlocks := (jsonObject.Info.PiecesLen + int64(BLOCK) - 1) / int64(BLOCK)
+		pieceOffset := int64(0)
 
-	} else {
-		fmt.Println("Unknown command: " + command)
+		for i := int64(0); i < numBlocks; i++ {
+			begin := pieceOffset + i*int64(BLOCK)
+			length := int64(BLOCK)
+
+			if i == numBlocks-1 {
+				length = jsonObject.Info.PiecesLen - begin
+			}
+			requestMessage := make([]byte, 13)
+			binary.BigEndian.PutUint32(requestMessage[:4], 13)
+			requestMessage[4] = 6
+			binary.BigEndian.PutUint32(requestMessage[5:9], uint32(pieceIndex))
+			binary.BigEndian.PutUint32(requestMessage[9:13], uint32(begin))
+			binary.BigEndian.PutUint32(requestMessage[13:17], uint32(length))
+
+			_, err := connections[peerStr].Write(requestMessage)
+			if err != nil {
+				fmt.Println("Error sending request message:", err)
+				return
+			}
+		}
+		combinedBlockPiece := make([]byte, jsonObject.Info.PiecesLen)
+		for i := int64(0); i < numBlocks; i++ {
+			data := handlePeerMessages(connections[peerStr], Piece)
+			pieceInd := binary.BigEndian.Uint32(data[1:5])
+			if pieceInd != uint32(pieceIndex) {
+				fmt.Println(err)
+				return
+			}
+			begin := binary.BigEndian.Uint32(data[5:9])
+			blockData := data[9:]
+			copy(combinedBlockPiece[begin:], blockData)
+		}
+		sum := sha1.Sum(combinedBlockPiece)
+		if string(sum[:]) == piecesHash {
+			err := os.WriteFile(os.Args[3], combinedBlockPiece, os.ModePerm)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+		} else {
+			fmt.Println("Unequal pieces")
+		}
+
+	default:
+		fmt.Println("Unknown command:", command)
 		os.Exit(1)
 	}
 }
