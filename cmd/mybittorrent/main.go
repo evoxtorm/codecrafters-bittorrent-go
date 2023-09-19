b9a1570b96ef815d5e6b1f8e1c5ef5fb9531d10f
evoxtorm <evostorm96@gmail.com>
2023-09-19 23:46:59 +0530

    feat: add code for stage 10

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -628,7 +628,7 @@ func main() {
 			copy(combinedBlockPiece[begin:], blockData)
 		}
 		sum := sha1.Sum(combinedBlockPiece)
-		fmt.Println(string(sum[:]), piecesHash, "this is hash")
+		fmt.Println(string(sum[:]) == piecesHash, "this is hash")
 		if string(sum[:]) == piecesHash {
 			err := os.WriteFile(os.Args[3], combinedBlockPiece, os.ModePerm)
 			if err != nil {
