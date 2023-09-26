0982a48ca06162373e4da683e4eced998df4c7eb
evoxtorm <evostorm96@gmail.com>
2023-09-26 12:57:34 +0530

    feat: add code for stage 10

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -639,7 +639,7 @@ func main() {
 			// 	fmt.Println("Error sending request message: ", err)
 			// 	return
 			// }
-			sendPeerMessage(connections[peerStr], Request, requestMessage)
+			sendPeerMessage(connections[peerStr], uint8(Request), requestMessage)
 			data, err := handlePeerMessages(connections[peerStr], Piece)
 			if err != nil {
 				panic(err)
